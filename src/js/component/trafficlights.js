0f42ce3cceba13c6a51be04c702a05a9a62152cb
diegomarteens <diegomarteens@gmail.com>
2021-11-19 00:35:46 +0000

    update1

---
+++ src/js/component/trafficlights.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export function TrafficLights() {
+	const [color, setColor] = React.useState;
+
+	return (
+		<div>
+			<div
+				onClick={() => {
+					setColor("");
+				}}
+				className={
+					"hookoflight" + (color === "null" ? " selected" : "")
+				}></div>
+			<div className="maintraffilight">
+				<div
+					onClick={() => {
+						setColor("red");
+					}}
+					className={
+						"red" + (color === "red" ? " selected" : "")
+					}></div>
+				<div
+					onClick={() => {
+						setColor("yellow");
+					}}
+					className={
+						"yellow" + (color === "yellow" ? " selected" : "")
+					}></div>
+				<div
+					onClick={() => {
+						setColor("green");
+					}}
+					className={
+						"green" + (color === "green" ? " selected" : "")
+					}></div>
+			</div>
+		</div>
+	);
+}
